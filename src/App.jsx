a161f609db90ff88cca350ec89b0d9a2b85975f8
Hamza3357 <hamzaawan3357@gmail.com>
2024-06-11 23:50:07 +0500

    form place set

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom';
-import Forms from './assets/Forms';
+import Forms from './Components/Forms';
 
 const App = () => {
 
